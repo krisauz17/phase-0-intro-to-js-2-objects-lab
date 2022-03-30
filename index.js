fd9ea634c18fd0dc9abd4339da49a250751c50b9
krishnendu basak <krisauz17@yahoo.com>
2022-03-31 00:00:58 +1100

    Object lab is done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+const employee = {
+    name:'Krishenndu',
+    streetAddress:'10-12 Bresbane Street.'
+};
+employee;
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, {[key]: value})
+    //employee.name='John';
+    //employee.streetAddress='4 Lawson Street.';
+}
+employee;
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]=value;
+    return employee;
+    //const newEmployee = {...employee};
+    //newEmployee[name]= 'Adam';
+    //newEmployee[streetAddress]= '15 Pages Road.';
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    //const newObj = {...employee};
+    const newObj = Object.assign({}, employee);
+    delete newObj[key];
+    return newObj;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
